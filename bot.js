f46816216c1fe94dc7428f766b5d4e1aa2390831
Gee Brain <admin@gozkybrain.com.ng>
2024-09-06 17:29:36 +0100

    Initial commit of backend project

---
+++ bot.js
@@ -0,0 +1,115 @@
+const http = require('http'); // Import the HTTP module, bcos of render deployment.
+const TelegramBot = require('node-telegram-bot-api');
+const { storeUserData } = require('./controllers/userController');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const botUrl = process.env.BOT_URL;
+
+let bot;
+
+const startBot = () => {
+    try {
+        bot = new TelegramBot(token, { polling: true });
+
+        bot.onText(/\/start/, async (msg) => {
+            const username = msg.from.username;
+            const userId = msg.from.id;
+
+            try {
+                await storeUserData(username, userId);
+                console.log('User data stored successfully');
+            } catch (error) {
+                console.error('Error storing user data:', error);
+            }
+
+            const webAppUrl = `${botUrl}/${username}/${userId}`;
+
+            const options = {
+                reply_markup: {
+                    inline_keyboard: [[
+                        {
+                            text: 'Open Web App',
+                            web_app: { url: webAppUrl }
+                        }
+                    ]]
+                }
+            };
+
+            try {
+                await bot.sendMessage(userId, `Welcome to JayGee, ${username} - ${userId}. Click the button below to proceed`, options);
+                console.log(`Sent message with link button to user ${username}`);
+            } catch (error) {
+                console.error('Error sending message:', error);
+            }
+        });
+
+        bot.onText(/\/connect/, async (msg) => {
+            const username = msg.from.username;
+            const userId = msg.from.id;
+
+            try {
+                await storeUserData(username, userId);
+                console.log('User data stored successfully');
+            } catch (error) {
+                console.error('Error storing user data:', error);
+            }
+
+            const serverUrl = `${botUrl}/${username}/${userId}`;
+
+
+            const options = {
+                reply_markup: {
+                    inline_keyboard: [[
+                        { text: 'Click here to proceed', url: serverUrl }
+                    ]]
+                }
+            };
+
+            try {
+                await bot.sendMessage(userId, `Welcome to JayGee, ${username}. Click the button below to proceed`, options);
+                console.log(`Sent message with link button to user ${username}`);
+            } catch (error) {
+                console.error('Error sending message:', error);
+            }
+        });
+
+        bot.on('polling_error', (error) => {
+            console.error('Polling error:', error);
+            console.log('Bot encountered an error and will restart...');
+            setTimeout(startBot, 10000); // Restart the bot after 10 seconds
+        });
+
+        console.log('Bot started');
+    } catch (error) {
+        console.error('Bot failed to start:', error);
+        console.log('Retrying bot startup in 10 seconds...');
+        setTimeout(startBot, 10000); // Retry after 10 seconds if an error occurs during startup
+    }
+};
+
+// Simple HTTP server to keep Render happy
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running\n');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+// Catch unhandled exceptions and restart the bot
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    console.log('Restarting bot...');
+    startBot();
+});
+
+startBot();
+
+module.exports = bot;
+
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = require('./app'); // Assuming your main app logic is in app.js
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORTS || 2020;
+
+// Define a basic route to check if the server is running
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
